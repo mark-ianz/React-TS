e070628350469c651e811cb36862d85c949b4f6b
[mark-ianz] <bustillomarkian23@gmail.com>
2024-08-21 11:03:00 +0800

    Added Guess the word (WIP)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import Button from "./components/Button";
 import Modal from "./components/Modal";
+import words from "./words";
 
 const App = () => {
   const name = "Ian";
-
   const test = (name: String = "tite", age: Number = 12) => {
     return console.log(name, age);
   };
@@ -14,6 +14,9 @@ const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [counter, setCounter] = useState(0);
 
+  const [wordToGuess, setWordToGuess] = useState(
+    () => words[Math.floor(Math.random() * words.length)]
+  );
   return (
     <div>
       {name}
@@ -29,6 +32,15 @@ const App = () => {
       <p>Counter: {counter}</p>
       <Button onClick={() => setCounter((c) => c - 1)}>Minus Count</Button>
       <Button onClick={() => setCounter((c) => c + 1)}>Plus Count</Button>
+      <div>
+        <h1 className="mb-4">Guess the word</h1>
+        <div className="flex gap-2">
+          {wordToGuess.split ("").map ((_, index)=> (
+            <span key={index} className="border-b border-black w-4"></span>
+          ))}
+        </div>
+        <input type="text" className="border-b border-black outline-none mt-2" />
+      </div>
     </div>
   );
 };
